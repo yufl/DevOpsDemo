1c91c07bfbd84b32ece96df84dca4f2c05043487
Yu, Feilong <feilong.yu@kuka.com>
2019-05-22 10:52:56 +0800

    Debug modules

--- src/com/kuka/App.py
+++ src/com/kuka/App.py
@@ -1,38 +1,17 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+'''
+Desc: Main program
+'''
 
-from win32com import client
+from src.com.kuka.util import utils
 import os
-import sys
 from docxtpl import Document
 
-def update_doc_toc(doc_path):
-    word = client.DispatchEx("Word.Application")
-    doc = word.Documents.Open(doc_path)
-    doc.TablesOfContents(1).Update()
-    doc.Close(SaveChanges=True)
-    word.Quit()
-    return
-
-def get_template_dir():
-    cur_dir = os.path.realpath(sys.argv[0])
-    temp_dir = cur_dir
-    dirs = cur_dir.split('\\')
-    src_index = 0
-    for i in range(len(dirs) - 1, -1, -1):
-        if dirs[i] == 'src':
-            src_index = i
-            break
-    if src_index == 0:
-        print('Wrong package.')
-    for i in range(src_index, len(dirs)):
-        temp_dir = temp_dir.replace(os.path.sep + dirs[i], '')
-    return temp_dir
-
 if __name__ == "__main__":
-    template_dir = os.path.join(get_template_dir(), 'templates')
+    template_dir = os.path.join(utils.get_template_dir(), 'templates')
     template_path = os.path.join(template_dir, "debug.docx")
     object_path = os.path.join(template_dir, "c:\\debugDemo.docx")
     doc = Document(template_path)
     doc.save(object_path)
-    update_doc_toc(object_path)
+    utils.update_doc_toc(object_path)
---
+++ src/com/kuka/util/utils.py
@@ -0,0 +1,26 @@
+from win32com import client
+import os
+import sys
+
+def update_doc_toc(doc_path):
+    word = client.DispatchEx("Word.Application")
+    doc = word.Documents.Open(doc_path)
+    doc.TablesOfContents(1).Update()
+    doc.Close(SaveChanges=True)
+    word.Quit()
+    return
+
+def get_template_dir():
+    cur_dir = os.path.realpath(sys.argv[0])
+    temp_dir = cur_dir
+    dirs = cur_dir.split('\\')
+    src_index = 0
+    for i in range(len(dirs) - 1, -1, -1):
+        if dirs[i] == 'src':
+            src_index = i
+            break
+    if src_index == 0:
+        print('Wrong package.')
+    for i in range(src_index, len(dirs)):
+        temp_dir = temp_dir.replace(os.path.sep + dirs[i], '')
+    return temp_dir
