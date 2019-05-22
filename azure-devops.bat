c834981944c0918cab5bf5fc529700f8df82a790
Yu, Feilong <feilong.yu@kuka.com>
2019-05-22 16:53:41 +0800

    Add azure package copy function

---
+++ azure-devops.bat
@@ -0,0 +1,8 @@
+pip show azure-devops > a.txt
+for /f "tokens=2* delims= " %%a in ('call findstr "Location" a.txt') do (set azure-devops=%%a)
+echo %azure-devops%
+dir .
+del a.txt
+mkdir .\\dist\\App\\azure
+xcopy /E %azure-devops%\\azure .\\dist\\App\\azure
+tree .\\dist\\App\\azure
