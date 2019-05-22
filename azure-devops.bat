abcbee600bc0b4b1bc1e3350843605b3cbdcb8e4
Yu, Feilong <feilong.yu@kuka.com>
2019-05-22 17:10:18 +0800

    Fix copy issue

--- azure-devops.bat
+++ azure-devops.bat
@@ -5,5 +5,6 @@ dir .
 del a.txt
 mkdir .\\dist\\App\\azure
 xcopy /E %azure-devops%\\azure .\\dist\\App\\azure
+mkdir .\\dist\\App\\templates
 xcopy /E .\\templates .\\dist\\App\\templates
 dir .\\dist\\App
